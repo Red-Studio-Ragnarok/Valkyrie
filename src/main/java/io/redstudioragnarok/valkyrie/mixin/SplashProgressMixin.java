bdaf56c3dc4114447604c61fad466afb8c84fac2
Desoroxxx <desoroxxx@gmail.com>
2023-06-11 22:57:14 +0200

    Allow bigger atlases

---
+++ src/main/java/io/redstudioragnarok/valkyrie/mixin/SplashProgressMixin.java
@@ -0,0 +1,34 @@
+package io.redstudioragnarok.valkyrie.mixin;
+
+import net.minecraftforge.fml.client.SplashProgress;
+import org.lwjgl.opengl.GL11;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import static io.redstudioragnarok.valkyrie.utils.ModReference.NAME;
+import static net.minecraftforge.fml.common.FMLLog.log;
+
+@Mixin(value = SplashProgress.class, remap = false)
+public class SplashProgressMixin {
+
+    @Shadow(remap = false) private static int max_texture_size;
+
+    /**
+     * @reason Actually get the max texture size, the OpenGL implementation supports, potentially allowing for bigger atlases.
+     * @author Desoroxxx
+     */
+    @Overwrite(remap = false)
+    public static int getMaxTextureSize() {
+        if (max_texture_size != -1)
+            return max_texture_size;
+
+        log.debug(NAME + ":" + "Max texture size isn't cached, getting it and caching...");
+
+        max_texture_size = GL11.glGetInteger(GL11.GL_MAX_TEXTURE_SIZE);
+
+        log.debug(NAME + ":" + "Max texture size is " + max_texture_size);
+
+        return max_texture_size;
+    }
+}
