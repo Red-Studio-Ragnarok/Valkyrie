4c5ce1242a707764a835beaef28f588a4074bfb5
Desoroxxx <desoroxxx@gmail.com>
2023-06-14 22:41:16 +0200

    Removed duplicated MixinBooter version checking from the F3 menu

--- src/main/java/io/redstudioragnarok/valkyrie/handlers/DebugHandler.java
+++ src/main/java/io/redstudioragnarok/valkyrie/handlers/DebugHandler.java
@@ -4,18 +4,14 @@ import io.redstudioragnarok.valkyrie.config.ValkyrieConfig;
 import io.redstudioragnarok.valkyrie.utils.ModReference;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.ArrayList;
 
 import static io.redstudioragnarok.valkyrie.Valkyrie.mc;
-import static io.redstudioragnarok.valkyrie.utils.ModReference.LATEST_MIXIN_BOOTER;
 
 public class DebugHandler {
-
-    private static final String MIXIN_BOOTER_VERSION = Loader.instance().getIndexedModList().get("mixinbooter").getVersion();
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onDebugList(RenderGameOverlayEvent.Text event) {
@@ -28,7 +24,7 @@ public class DebugHandler {
             if (!debugScreenLeft.get(debugScreenLeft.size() - 1).equals(""))
                 debugScreenLeft.add("");
 
-            debugScreenLeft.add(String.format("%s<Valkyrie>%s Valkyrie version is %s, Mixin Booter is %sup to date (%s).", TextFormatting.RED, TextFormatting.RESET, ModReference.VERSION, MIXIN_BOOTER_VERSION.equals(LATEST_MIXIN_BOOTER) ? "" : "not ", MIXIN_BOOTER_VERSION));
+            debugScreenLeft.add(String.format("%s<Valkyrie>%s Valkyrie version is %s.", TextFormatting.RED, TextFormatting.RESET, ModReference.VERSION));
             debugScreenLeft.add("");
             if (ValkyrieConfig.clouds.enabled)
                 debugScreenLeft.add(String.format("%s<Valkyrie>%s Clouds are enabled, render distance: %s, height: %s, layers: %s,  wireframe is %s", TextFormatting.RED, TextFormatting.RESET, ValkyrieConfig.clouds.renderDistance, ValkyrieConfig.clouds.height, ValkyrieConfig.clouds.layers, ValkyrieConfig.debug.wireframeClouds ? "on" : "off"));
