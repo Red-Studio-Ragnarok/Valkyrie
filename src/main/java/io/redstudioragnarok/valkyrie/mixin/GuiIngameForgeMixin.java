836a8fb56042b30a9063d99dfd0af7203ee45e57
Desoroxxx <desoroxxx@gmail.com>
2023-06-15 00:02:52 +0200

    Use `ModifyArg` instead of `Redirect` to fix MC-67532
    Much, much cleaner thanks @embeddedt for the discovery

--- src/main/java/io/redstudioragnarok/valkyrie/mixin/GuiIngameForgeMixin.java
+++ src/main/java/io/redstudioragnarok/valkyrie/mixin/GuiIngameForgeMixin.java
@@ -1,56 +1,45 @@
 package io.redstudioragnarok.valkyrie.mixin;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiIngame;
 import net.minecraftforge.client.GuiIngameForge;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(GuiIngameForge.class)
-public abstract class GuiIngameForgeMixin extends GuiIngame {
+public abstract class GuiIngameForgeMixin {
 
-    private GuiIngameForgeMixin(Minecraft mcIn) {super(mcIn);}
-
-    @Shadow(remap = false) protected abstract void renderArmor(int width, int height);
-    @Shadow(remap = false) protected abstract void renderHealthMount(int width, int height);
-    @Shadow(remap = false) protected abstract void renderAir(int width, int height);
-    @Shadow(remap = false) protected abstract void renderJumpBar(int width, int height);
-    @Shadow(remap = false) protected abstract void renderExperience(int width, int height);
-
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderHealth(II)V", remap = false))
-    private void raiseHealth(final GuiIngameForge guiIngameForge, final int width, final int height) {
-        guiIngameForge.renderHealth(width, height - 3);
+    @ModifyArg(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderHealth(II)V", remap = false), index = 1)
+    private int raiseHealth(final int original) {
+        return original - 3;
     }
 
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderArmor(II)V", remap = false))
-    private void raiseArmor(final GuiIngameForge guiIngameForge, final int width, final int height) {
-        renderArmor(width, height - 3);
+    @ModifyArg(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderArmor(II)V", remap = false), index = 1)
+    private int raiseArmor(final int original) {
+        return original - 3;
     }
 
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderFood(II)V", remap = false))
-    private void raiseFood(final GuiIngameForge guiIngameForge, final int width, final int height) {
-        guiIngameForge.renderFood(width, height - 3);
+    @ModifyArg(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderFood(II)V", remap = false), index = 1)
+    private int raiseFood(final int original) {
+        return original - 3;
     }
 
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderHealthMount(II)V", remap = false))
-    private void raiseHealthMount(final GuiIngameForge guiIngameForge, final int width, final int height) {
-        renderHealthMount(width, height - 3);
+    @ModifyArg(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderHealthMount(II)V", remap = false), index = 1)
+    private int raiseHealthMount(final int original) {
+        return original - 3;
     }
 
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderAir(II)V", remap = false))
-    private void raiseAir(final GuiIngameForge guiIngameForge, final int width, final int height) {
-        renderAir(width, height - 3);
+    @ModifyArg(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderAir(II)V", remap = false), index = 1)
+    private int raiseAir(final int original) {
+        return original - 3;
     }
 
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderJumpBar(II)V", remap = false))
-    private void raiseJumpBar(final GuiIngameForge guiIngameForge, final int width, final int height) {
-        renderJumpBar(width, height - 3);
+    @ModifyArg(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderJumpBar(II)V", remap = false), index = 1)
+    private int raiseJumpBar(final int original) {
+        return original - 3;
     }
 
-    @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderExperience(II)V", remap = false))
-    private void raiseExperience(final GuiIngameForge guiIngameForge, final int width, final int height) {
-        renderExperience(width, height - 3);
+    @ModifyArg(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/GuiIngameForge;renderExperience(II)V", remap = false), index = 1)
+    private int raiseExperience(final int original) {
+        return original - 3;
     }
 }
--- src/main/java/io/redstudioragnarok/valkyrie/mixin/appleskin/HUDOverlayHandlerMixin.java
+++ src/main/java/io/redstudioragnarok/valkyrie/mixin/appleskin/HUDOverlayHandlerMixin.java
@@ -1,26 +1,25 @@
 package io.redstudioragnarok.valkyrie.mixin.appleskin;
 
-import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import squeek.appleskin.client.HUDOverlayHandler;
 
 @Mixin(value = HUDOverlayHandler.class, remap = false)
 public class HUDOverlayHandlerMixin {
 
-    @Redirect(method = "onPreRender", at = @At(value = "INVOKE", target = "Lsqueek/appleskin/client/HUDOverlayHandler;drawExhaustionOverlay(FLnet/minecraft/client/Minecraft;IIF)V", remap = false), remap = false)
-    private void raiseExhaustionOverlay(final float exhaustion, final Minecraft mc, final int left, final int top, final float alpha) {
-        HUDOverlayHandler.drawExhaustionOverlay(exhaustion, mc, left, top -3, alpha);
+    @ModifyArg(method = "onPreRender", at = @At(value = "INVOKE", target = "Lsqueek/appleskin/client/HUDOverlayHandler;drawExhaustionOverlay(FLnet/minecraft/client/Minecraft;IIF)V", remap = false), index = 3, remap = false)
+    private int raiseExhaustionOverlay(final int original) {
+        return original - 3;
     }
 
-    @Redirect(method = "onRender", at = @At(value = "INVOKE", target = "Lsqueek/appleskin/client/HUDOverlayHandler;drawSaturationOverlay(FFLnet/minecraft/client/Minecraft;IIF)V", remap = false), remap = false)
-    private void raiseSaturationOverlay(final float saturationGained, final float saturationLevel, final Minecraft mc, final int left, final int top, final float alpha) {
-        HUDOverlayHandler.drawSaturationOverlay(saturationGained, saturationLevel, mc, left, top -3, alpha);
+    @ModifyArg(method = "onRender", at = @At(value = "INVOKE", target = "Lsqueek/appleskin/client/HUDOverlayHandler;drawSaturationOverlay(FFLnet/minecraft/client/Minecraft;IIF)V", remap = false), index = 4, remap = false)
+    private int raiseSaturationOverlay(final int original) {
+        return original - 3;
     }
 
-    @Redirect(method = "onRender", at = @At(value = "INVOKE", target = "Lsqueek/appleskin/client/HUDOverlayHandler;drawHungerOverlay(IILnet/minecraft/client/Minecraft;IIF)V", remap = false), remap = false)
-    private void raiseHungerOverlay(final int hungerRestored, final int foodLevel, final Minecraft mc, final int left, final int top, final float alpha) {
-        HUDOverlayHandler.drawHungerOverlay(hungerRestored, foodLevel, mc, left, top -3, alpha);
+    @ModifyArg(method = "onRender", at = @At(value = "INVOKE", target = "Lsqueek/appleskin/client/HUDOverlayHandler;drawHungerOverlay(IILnet/minecraft/client/Minecraft;IIF)V", remap = false), index = 4, remap = false)
+    private int raiseHungerOverlay(final int original) {
+        return original - 3;
     }
 }
