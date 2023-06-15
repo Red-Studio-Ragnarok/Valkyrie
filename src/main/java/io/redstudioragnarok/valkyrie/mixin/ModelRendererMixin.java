28103b98e57d41df2c4d6656623a66c800904e75
Desoroxxx <desoroxxx@gmail.com>
2023-06-15 19:52:02 +0200

    Revert "Only update the `rotationMatrix` when needed"
    This reverts commit 56a8527bc8c4f87b029972963860ca301c069f24.

--- src/main/java/io/redstudioragnarok/valkyrie/mixin/ModelRendererMixin.java
+++ src/main/java/io/redstudioragnarok/valkyrie/mixin/ModelRendererMixin.java
@@ -2,8 +2,8 @@ package io.redstudioragnarok.valkyrie.mixin;
 
 import net.jafama.FastMath;
 import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.client.renderer.GlStateManager;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -33,12 +33,6 @@ public class ModelRendererMixin {
 
     private final FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
 
-    private float prevRotateAngleX;
-    private float prevRotateAngleY;
-    private float prevRotateAngleZ;
-
-    private float[] rotationMatrix = new float[16];
-
     /**
      *
      * @reason Improving performance, this updated implementation utilizes a rotation matrix to handle all rotation transformations at once, thus reducing the number of trigonometric computations and OGL calls. Moreover, it utilizes a buffer to directly load the rotation matrix into OGL.
@@ -46,43 +40,36 @@ public class ModelRendererMixin {
      */
     @Overwrite
     public void render(float scale) {
-        if (isHidden || !showModel)
+        if (this.isHidden || !this.showModel)
             return;
 
-        if (!compiled)
-            compileDisplayList(scale);
+        if (!this.compiled)
+            this.compileDisplayList(scale);
 
-        GlStateManager.pushMatrix();
+        GL11.glPushMatrix();
 
-        GlStateManager.translate(rotationPointX * scale, rotationPointY * scale, rotationPointZ * scale);
+        GL11.glTranslatef(this.rotationPointX * scale, this.rotationPointY * scale, this.rotationPointZ * scale);
 
-        if (rotateAngleX != prevRotateAngleX || rotateAngleY != prevRotateAngleY || rotateAngleZ != prevRotateAngleZ) {
-            final float cosX = (float) FastMath.cos(rotateAngleX);
-            final float sinX = (float) FastMath.sin(rotateAngleX);
-            final float cosY = (float) FastMath.cos(rotateAngleY);
-            final float sinY = (float) FastMath.sin(rotateAngleY);
-            final float cosZ = (float) FastMath.cos(rotateAngleZ);
-            final float sinZ = (float) FastMath.sin(rotateAngleZ);
+        final float cosX = (float) FastMath.cos(this.rotateAngleX);
+        final float sinX = (float) FastMath.sin(this.rotateAngleX);
+        final float cosY = (float) FastMath.cos(this.rotateAngleY);
+        final float sinY = (float) FastMath.sin(this.rotateAngleY);
+        final float cosZ = (float) FastMath.cos(this.rotateAngleZ);
+        final float sinZ = (float) FastMath.sin(this.rotateAngleZ);
 
-            rotationMatrix = new float[]{cosY * cosZ, cosY * sinZ, -sinY, 0, sinX * sinY * cosZ - cosX * sinZ, sinX * sinY * sinZ + cosX * cosZ, sinX * cosY, 0, cosX * sinY * cosZ + sinX * sinZ, cosX * sinY * sinZ - sinX * cosZ, cosX * cosY, 0, 0, 0, 0, 1};
+        final float[] rotationMatrix = {cosY * cosZ, cosY * sinZ, -sinY, 0, sinX * sinY * cosZ - cosX * sinZ, sinX * sinY * sinZ + cosX * cosZ, sinX * cosY, 0, cosX * sinY * cosZ + sinX * sinZ, cosX * sinY * sinZ - sinX * cosZ, cosX * cosY, 0, 0, 0, 0, 1};
 
-            buffer.put(rotationMatrix).flip();
+        buffer.put(rotationMatrix).flip();
+        GL11.glMultMatrix(buffer);
 
-            prevRotateAngleX = rotateAngleX;
-            prevRotateAngleY = rotateAngleY;
-            prevRotateAngleZ = rotateAngleZ;
-        }
+        GL11.glCallList(this.displayList);
 
-        GlStateManager.multMatrix(buffer);
-
-        GlStateManager.callList(displayList);
-
-        if (childModels != null)
-            for (ModelRenderer childModel : childModels)
+        if (this.childModels != null)
+            for (ModelRenderer childModel : this.childModels)
                 childModel.render(scale);
 
-        GlStateManager.popMatrix();
+        GL11.glPopMatrix();
 
-        GlStateManager.translate(-offsetX, -offsetY, -offsetZ);
+        GL11.glTranslatef(-this.offsetX, -this.offsetY, -this.offsetZ);
     }
 }
