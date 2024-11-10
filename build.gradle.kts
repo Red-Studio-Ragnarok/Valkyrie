542b4730e491a05cea943b3b85663ac15730b48d
Luna Mira Lage (Desoroxxx) <desoroxxx@gmail.com>
2024-11-10 17:25:26 +0100

    Downgrade `io.freefair.lombok`

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("com.gtnewhorizons.retrofuturagradle") version "1.3.34"
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.9"
     id("com.github.gmazzo.buildconfig") version "5.5.0"
-    id("io.freefair.lombok") version "8.10"
+    id("io.freefair.lombok") version "8.7.1"
 }
 
 group = "dev.redstudio"
