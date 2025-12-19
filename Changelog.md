39f40195f98ce1f3c3881ba956b65e9ddf519bf4
Luna Mira Lage (Desoroxxx) <desoroxxx.git@redstudio.dev>
2025-12-19 06:26:32 +0100

    Consistently use `GlStateManager`

--- Changelog.md
+++ Changelog.md
@@ -15,6 +15,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 - Simplify `valkyrieIteration`
 - Valkyrie now removes MipMap level options
 - Updated credits
+- Consistently use `GlStateManager`
 
 ### Fixed
 
--- src/main/java/dev/redstudio/valkyrie/mixin/ModelRendererMixin.java
+++ src/main/java/dev/redstudio/valkyrie/mixin/ModelRendererMixin.java
@@ -6,7 +6,6 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.joml.Matrix4f;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -163,6 +162,6 @@ public abstract class ModelRendererMixin {
 		}
 
 		valkyrie$matrix.get(valkyrie$buffer);
-		GL11.glMultMatrix(valkyrie$buffer);
+		GlStateManager.multMatrix(valkyrie$buffer);
 	}
 }
