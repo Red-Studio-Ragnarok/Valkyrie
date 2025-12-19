38423f96ad2e871180c0cd515c3cebe0ca76b2d7
Luna Mira Lage (Desoroxxx) <desoroxxx.git@redstudio.dev>
2025-12-19 06:31:55 +0100

    Improved `SplashProgress#getMaxTextureSize`

--- src/main/java/dev/redstudio/valkyrie/mixin/SplashProgressMixin.java
+++ src/main/java/dev/redstudio/valkyrie/mixin/SplashProgressMixin.java
@@ -10,25 +10,17 @@ import static dev.redstudio.valkyrie.ProjectConstants.NAME;
 import static net.minecraftforge.fml.common.FMLLog.log;
 
 @Mixin(value = SplashProgress.class, remap = false)
-public class SplashProgressMixin {
+public final class SplashProgressMixin {
 
 	@Shadow(remap = false)
 	private static int max_texture_size;
 
-	/**
-	 * @reason Actually get the max texture size the OpenGL implementation supports, potentially allowing for bigger atlases on certain GPUs.
-	 * @author Desoroxxx
-	 */
+	/// @reason Actually get the max texture size the OpenGL implementation supports, potentially allowing for bigger atlases on certain GPUs.
+	/// @author Luna Mira Lage (Desoroxxx)
 	@Overwrite(remap = false)
 	public static int getMaxTextureSize() {
-		if (max_texture_size != -1)
-			return max_texture_size;
-
-		log.debug(NAME + ":" + "Max texture size isn't cached, getting it and caching...");
-
-		max_texture_size = GL11.glGetInteger(GL11.GL_MAX_TEXTURE_SIZE);
-
-		log.debug(NAME + ":" + "Max texture size is " + max_texture_size);
+		if (max_texture_size == -1)
+			max_texture_size = GL11.glGetInteger(GL11.GL_MAX_TEXTURE_SIZE);
 
 		return max_texture_size;
 	}
